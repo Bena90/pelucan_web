bee9e97d89872f6277ea426290da7c1d825fe7be
Bena90 <esteban.benabarre@gmail.com>
2023-06-17 02:36:28 -0300

    delete metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
 "use client"
 import { Divider } from '@chakra-ui/react'
-import { Metadata } from 'next'
 import ArticleList from './components/about/about'
 import Contact from './components/contact/contact'
 import Feature from './components/feature/feature'
 import Hero from './components/hero/hero'
-
-export const metadata: Metadata = {
-  title: 'Pelucan - Sergio Velazquez Groomer',
-  description: 'Pelucan - Sergio Velazquez Groomer',
-}
 
 export default function Home() {
   return (
